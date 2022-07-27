5128c96e5d52f4c7a46305ea0af12200676ea8be
Sam Tomashi <samtomashikayembi@yahoo.fr>
2022-07-27 10:55:48 +0300

    Implement update todolist

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 function TodoList({todoList, deleteTodo}){
+    // const [description, updateDescription] = useState()
+
+    function updateDescription(event){
+        const description = event.target.value;
+        const foundList = todoList.filter((item)=> item.id == event.target.id);
+        foundList[0]['description'] = description;
+
+        todoList = [...foundList];
+    }
+
+    console.log(todoList);
 
     const listItem = todoList.map(
         (item)=> 
-        <li key={item.id}>{item.description} 
-        <button className="btn btn-primary" onClick={()=> deleteTodo(item.id)}>DELETE</button></li> 
+        <li key={item.id}>
+            <input id={item.id} type="text" className="m-2 " onChange={updateDescription} value={item.description} ></input>
+            <button className="btn btn-sm btn-primary m-2" onClick={()=> deleteTodo(item.id)}>DELETE</button>
+        </li> 
     )
     return (
-        <div>
+        <ol>
             {listItem}
-        </div>
+        </ol>
     )
 }
 
